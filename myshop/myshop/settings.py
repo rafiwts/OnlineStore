3e337ac27481760b2aacc510006cda897a72c473
Rafał Krupiński <rafiwts@gmail.com>
2023-05-19 17:26:14 +0200

    Adding a path to a context processor file for an app

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
