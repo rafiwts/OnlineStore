39d47a302d7048b138fcb0ac594d91ceaef359f9
Rafał Krupiński <rafiwts@gmail.com>
2023-05-21 19:34:24 +0200

    Adding celery to the project

---
+++ myshop/myshop/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+
+app = Celery('myshop')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app._autodiscover_tasks(lambda: settings.INSTALLED_APPS, related_name='CELERY')
