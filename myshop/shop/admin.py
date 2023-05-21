ca404a9fa62ca9c2a6b136dc2021405fe66f3c74
Rafał Krupiński <rafiwts@gmail.com>
2023-05-21 19:36:56 +0200

    Adding category and product to an admin site

---
+++ myshop/shop/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} # value insterted automatically on the basis of other columns
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available'] # can be editted from the admin site (they need to be in list_display)
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
