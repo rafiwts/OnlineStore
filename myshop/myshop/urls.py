07b977ff4eb84e0cef943c7b1de33fef120bdeb4
Rafał Krupiński <rafiwts@gmail.com>
2023-05-28 20:02:05 +0200

    Adding a coupon path to the url file

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -23,10 +23,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
     path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
+    path('coupons/', include(('coupons.urls', 'coupons'), namespace='coupons')),
     path('', include(('shop.urls', 'shop'), namespace='shop'))
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, 
                           document_root=settings.MEDIA_ROOT)
-    
